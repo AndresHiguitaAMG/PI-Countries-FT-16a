ea8c015238022f57546c70521a16e5c4a8647475
AndresHiguitaAMG <andreshiguitaamg@gmail.com>
2022-03-23 20:22:04 -0500

    404

--- api/src/controllers/countriesController.js
+++ api/src/controllers/countriesController.js
@@ -4,21 +4,33 @@ const { Activity, Country, Op } = require('../db');
 const getAllCountries = async (req, res, next) => {
     try {
         const { name } = req.query;
+        let allData = [];
         if (name) {
             const responseByName = await Country.findAll({
                 attributes: ["flag", "name", "continent"],
+                through: {
+                    attributes: []
+                },
                 where: {
                     name: {
-                        [Op.iLike]: `%${name}%`,
+                        [Op.iLike]: `%${name}%`
                     }
                 }
             });
-            return res.json(responseByName);
+            allData = responseByName;
+        } else {
+            const myInformationDb = await Country.findAll({
+                attributes: ["flag", "name", "continent"],
+                through: {
+                    attributes: []
+                },
+            });
+            allData = myInformationDb;
         }
-        const myInformationDb = await Country.findAll({
-            attributes: ["flag", "name", "continent"]
-        });
-        return res.json(myInformationDb);
+        if (allData.length === 0) {
+            return res.status(404).json({message: "Not Found"});
+        }
+        return res.json(allData);
     } catch (error) {
         next (error);
     };
