08581ae64d5c6d5409f1aa19d1a05a2acbe7341f
AndresHiguitaAMG <andreshiguitaamg@gmail.com>
2022-03-25 21:49:16 -0500

    back end finish

--- api/src/controllers/activitiesController.js
+++ api/src/controllers/activitiesController.js
@@ -1,10 +1,21 @@
-const { Activity, Country } = require('../db');
+const { Activity, Country, Op } = require('../db');
 
 const postActivities = async (req, res, next) => {
     try {
-        
+        const { name, dificulty, duration, season, countries  } = req.body
+
+  const newActivity = await Activity.create({
+    name,
+    dificulty,
+    duration,
+    season,
+  });
+  countries.map(async pr => {
+    await newActivity.setCountries(await Country.findByPk(pr));
+  });
+  return res.json(newActivity);
     } catch (error) {
-        next (error);
+        next (error); 
     };
 };
 
--- api/src/models/Activity.js
+++ api/src/models/Activity.js
@@ -15,7 +15,7 @@ module.exports = (sequelize) => {
       }
     },
     duration: {
-      type: DataTypes.STRING
+      type: DataTypes.INTEGER
     },
     season: {
       type: DataTypes.ENUM("summer", "autumn", "winter", "spring")
