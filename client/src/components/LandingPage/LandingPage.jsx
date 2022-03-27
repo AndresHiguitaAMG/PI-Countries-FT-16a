1aa6f4a2f100fd5e669854045082e8a008040c9a
AndresHiguitaAMG <andreshiguitaamg@gmail.com>
2022-03-27 11:59:48 -0500

    added components on the front end

---
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+ 
+const LandingPage = () => {
+     return (
+         <div>
+             <link to="/home">Welcome Aboard</link>
+         </div>
+     );
+ }
+  
+ export default LandingPage;
